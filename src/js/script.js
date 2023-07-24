5e89cf3c8dfd5730189110ec7da364ceeccbdc3a
Samira <samira.ibrhmv@gmail.com>
2023-07-24 17:09:43 +0400

    tabs added

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,16 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
+
+(function($) {
+    $(function() {
+      
+      $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+      
+    });
+    })(jQuery);
